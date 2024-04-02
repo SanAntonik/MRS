c02a02a77b2d68ebaeb1c160b5135777303d4cda
SanAntonik <sanantonik@gmail.com>
2024-04-02 13:03:59 +0300

    Complete simple POST and GET ALL requests for testing purposes

--- main.py
+++ main.py
@@ -1,9 +1,29 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
 
 app = FastAPI()
 
 
+items = []
+
+
 @app.get("/")
 def home():
     return {"Data": "Test"}
+
+
+@app.get("/items", response_model=list[str])
+def get_all(limit: int = 10):
+    return items[0:limit]
+
+
+# @app.get("/items/{item_id}")
+# def get_by_id(item_id: int) -> str:
+#     item = items[item_id]
+#     return item
+
+
+@app.post("/items")
+def create_item(item: str):
+    items.append(item)
+    return items
