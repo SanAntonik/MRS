040b51bc8a164237ff8a4fa0acfbc1245b28d610
SanAntonik <sanantonik@gmail.com>
2024-04-12 10:54:55 +0300

    Make only admin be able to delete items

--- backend/app/api/routes/items.py
+++ backend/app/api/routes/items.py
@@ -97,8 +97,10 @@ def delete_item(session: SessionDep, current_user: CurrentUser, id: int) -> Mess
     item = session.get(Item, id)
     if not item:
         raise HTTPException(status_code=404, detail="Item not found")
-    if not current_user.is_superuser and (item.owner_id != current_user.id):
+    if not current_user.is_superuser:
         raise HTTPException(status_code=400, detail="Not enough permissions")
+    # if not current_user.is_superuser and (item.owner_id != current_user.id):
+    #     raise HTTPException(status_code=400, detail="Not enough permissions")
     session.delete(item)
     session.commit()
     return Message(message="Item deleted successfully")
