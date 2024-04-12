15451619af624d78792994077824f299f33e2b1e
SanAntonik <sanantonik@gmail.com>
2024-04-12 10:56:50 +0300

    Make only admin be able to update items

--- backend/app/api/routes/items.py
+++ backend/app/api/routes/items.py
@@ -79,8 +79,10 @@ def update_item(
     item = session.get(Item, id)
     if not item:
         raise HTTPException(status_code=404, detail="Item not found")
-    if not current_user.is_superuser and (item.owner_id != current_user.id):
+    if not current_user.is_superuser:
         raise HTTPException(status_code=400, detail="Not enough permissions")
+    # if not current_user.is_superuser and (item.owner_id != current_user.id):
+    #     raise HTTPException(status_code=400, detail="Not enough permissions")
     update_dict = item_in.model_dump(exclude_unset=True)
     item.sqlmodel_update(update_dict)
     session.add(item)
