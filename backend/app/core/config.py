fafb0ac002355d30e51542664b14172df7c881ed
SanAntonik <sanantonik@gmail.com>
2024-04-26 15:38:43 +0300

    Fix the problem of not being able to login due to no connection between frontend and backend. Remove redundant .env file in MRS/

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,35 +1,23 @@
 import secrets
-from typing import Annotated, Any
 
-from pydantic import (
-    AnyUrl,
-    BeforeValidator,
-    PostgresDsn,
-    computed_field,
-)
+from pydantic import AnyUrl, PostgresDsn, computed_field
 from pydantic_core import MultiHostUrl
 from pydantic_settings import BaseSettings
 
 
-def parse_cors(v: Any) -> list[str] | str:
-    if isinstance(v, str) and not v.startswith("["):
-        return [i.strip() for i in v.split(",")]
-    elif isinstance(v, list | str):
-        return v
-    raise ValueError(v)
-
-
 class Settings(BaseSettings):
+    PROJECT_NAME: str = "MRS"
+    FIRST_SUPERUSER: str = "admin@example.com"
+    FIRST_SUPERUSER_PASSWORD: str = "admin"
+    USERS_OPEN_REGISTRATION: bool = True
     API_V1_STR: str = "/api/v1"
     SECRET_KEY: str = secrets.token_urlsafe(32)
     # 60 minutes * 24 hours * 8 days = 8 days
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 8
+    BACKEND_CORS_ORIGINS: list[AnyUrl] | str = [
+        "http://localhost:5173",
+    ]
 
-    BACKEND_CORS_ORIGINS: Annotated[
-        list[AnyUrl] | str, BeforeValidator(parse_cors)
-    ] = []
-
-    PROJECT_NAME: str = "MRS"
     POSTGRES_SERVER: str = "localhost"
     POSTGRES_PORT: int = 5432
     POSTGRES_USER: str = "postgres"
@@ -47,10 +35,6 @@ class Settings(BaseSettings):
             port=self.POSTGRES_PORT,
             path=self.POSTGRES_DB,
         )
-
-    FIRST_SUPERUSER: str = "admin@example.com"
-    FIRST_SUPERUSER_PASSWORD: str = "admin"
-    USERS_OPEN_REGISTRATION: bool = True
 
 
 settings = Settings()  # type: ignore
