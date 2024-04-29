edfdc4e6ae80e77c886cf7ea91ac18463d02444a
SanAntonik <sanantonik@gmail.com>
2024-04-29 23:43:16 +0300

    Make cosmetic changes in sidebar

--- frontend/src/components/Common/SidebarItems.tsx
+++ frontend/src/components/Common/SidebarItems.tsx
@@ -1,13 +1,14 @@
 import { Box, Flex, Icon, Text, useColorModeValue } from "@chakra-ui/react"
 import { Link } from "@tanstack/react-router"
-import { FiBriefcase, FiHome, FiSettings, FiUsers } from "react-icons/fi"
+import { FiFilm, FiHome, FiSettings, FiUsers } from "react-icons/fi"
+
 import { useQueryClient } from "@tanstack/react-query"
 
 import type { UserOut } from "../../client"
 
 const items = [
   { icon: FiHome, title: "Dashboard", path: "/" },
-  { icon: FiBriefcase, title: "Items", path: "/items" },
+  { icon: FiFilm, title: "Movies", path: "/items" },
   { icon: FiSettings, title: "User Settings", path: "/settings" },
 ]
 
