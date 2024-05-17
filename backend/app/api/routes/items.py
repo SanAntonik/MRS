46dcb2f37d1fc94d869db768296928910a17c4cd
SanAntonik <sanantonik@gmail.com>
2024-05-17 12:54:42 +0300

    Modify GET ALL items to return 100 items in descending order by sorted by id

--- backend/app/api/routes/items.py
+++ backend/app/api/routes/items.py
@@ -18,27 +18,8 @@ def read_items(
     """
     count_statement = select(func.count()).select_from(Item)
     count = session.exec(count_statement).one()
-    statement = select(Item).offset(skip).limit(limit)
+    statement = select(Item).offset(skip).limit(limit).order_by(Item.id.desc())
     items = session.exec(statement).all()
-    # if current_user.is_superuser:
-    #     count_statement = select(func.count()).select_from(Item)
-    #     count = session.exec(count_statement).one()
-    #     statement = select(Item).offset(skip).limit(limit)
-    #     items = session.exec(statement).all()
-    # else:
-    #     count_statement = (
-    #         select(func.count())
-    #         .select_from(Item)
-    #         .where(Item.owner_id == current_user.id)
-    #     )
-    #     count = session.exec(count_statement).one()
-    #     statement = (
-    #         select(Item)
-    #         .where(Item.owner_id == current_user.id)
-    #         .offset(skip)
-    #         .limit(limit)
-    #     )
-    #     items = session.exec(statement).all()
     return ItemsOut(data=items, count=count)
 
 
