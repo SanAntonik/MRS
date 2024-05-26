4858a9509733cb88274a70be547a2cab8b728c87
SanAntonik <sanantonik@gmail.com>
2024-05-26 11:05:17 +0300

    Change text messages in index.tsx

--- frontend/src/routes/_layout/index.tsx
+++ frontend/src/routes/_layout/index.tsx
@@ -18,9 +18,9 @@ function Dashboard() {
       <Container maxW="full">
         <Box pt={12} m={4}>
           <Text fontSize="2xl">
-            Hi, {currentUser?.full_name || currentUser?.email} 👋🏼
+            Hello, {currentUser?.full_name || currentUser?.email} 👋🏼
           </Text>
-          <Text>Welcome back, nice to see you again!</Text>
+          <Text>Welcome back to the Movie Recommendation System!</Text>
         </Box>
       </Container>
     </>
